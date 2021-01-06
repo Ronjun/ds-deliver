663b55adc7ee902395013886b4259e6556c0aacf
Ronaldo Junior <ronaldocosta2603@gmail.com>
2021-01-06 11:46:06 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,25 @@
+import { ReactComponent as LinkedInIcon } from "./linkedin.svg";
+import { ReactComponent as YoutubeIcon } from "./youtube.svg";
+import { ReactComponent as InstagramIcon } from "./instagram.svg";
+import "./styles.css";
+
+function Footer() {
+  return (
+    <footer className="main-footer">
+      App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+      <div className="footer-icons">
+        <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+          <YoutubeIcon />
+        </a>
+        <a href="https://www.linkedin.com/school/devsuperior/" target="new">
+          <LinkedInIcon />
+        </a>
+        <a href="https://www.instagram.com/school/devsuperior.ig/" target="new">
+          <InstagramIcon />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
